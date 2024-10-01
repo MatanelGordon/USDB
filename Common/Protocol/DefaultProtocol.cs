5b1713e03a4d1b9359731d02cbcaede32f9b52e5
Matanel Mac <mmgordon.matanel@gmail.com>
2024-10-01 14:16:24 +0300

    changed len algorithem and updated max message size to 4*10^7

--- Common/Protocol/DefaultProtocol.cs
+++ Common/Protocol/DefaultProtocol.cs
@@ -8,15 +8,13 @@ public class DefaultProtocol : IProtocol
 
     public async Task<byte[]> Unwrap(Stream stream)
     {
-        var lenMSB = stream.ReadByte();
-        var lenLSB = stream.ReadByte();
-        var len = lenMSB * 256 + lenLSB;
+        var len = ConstructLen(stream);
         var data = new byte[len];
 
-        await stream.ReadAsync(data);
+        await stream.ReadExactlyAsync(data);
 
         var crcResult = new byte[CRC32_SIZE_OVERHEAD];
-        await stream.ReadAsync(crcResult);
+        await stream.ReadExactlyAsync(crcResult);
 
         var redoCRC = Crc32.Hash(data);
 
@@ -32,13 +30,6 @@ public class DefaultProtocol : IProtocol
 
     public byte[] Wrap(byte[] data)
     {
-        var len = data.Length;
-
-        if (len > 256 * 256)
-        {
-            throw new Exception("Message Too Big");
-        }
-
         var crc32 = Crc32.Hash(data);
 
         if (crc32 is null)
@@ -46,13 +37,19 @@ public class DefaultProtocol : IProtocol
             throw new Exception("Failed to CRC32 Message");
         }
 
-        var lenLSB = (byte)(data.Length % 256);
-        var lenMSB = (byte)(data.Length >> 8);
-        var result = new List<byte>() { lenMSB, lenLSB };
+        var result = DeconstructLen(data.Length).ToList();
 
         result.AddRange(data);
         result.AddRange(crc32);
 
         return result.ToArray();
+    }
+
+    private byte[] DeconstructLen(int len) => BitConverter.GetBytes(len);
+
+    private int ConstructLen(Stream stream)
+    {
+        var bytes = new byte[4].Select(_ => (byte) stream.ReadByte()).ToArray();
+        return BitConverter.ToInt32(bytes);
     }
 }
